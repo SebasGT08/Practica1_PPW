4c99e4cc436420659c175058d2d4217941999e9a
SebasGT08 <guaman.sebastian802@gmail.com>
2023-04-19 11:55:11 -0500

    Estilo v3

--- js/comprobarEstilo.js
+++ js/comprobarEstilo.js
@@ -1,5 +1,6 @@
 // Verificar si hay un valor en localStorage con el nombre "hoja-estilo"
 var localStorageValue = localStorage.getItem("hoja-estilo");
+var localStorageValue2 = localStorage.getItem("menu");
 
 if (localStorageValue === "1") {
   // Si se ha guardado el valor "1", activar la primera hoja de estilo
@@ -13,4 +14,18 @@ if (localStorageValue === "1") {
   hojaEstilo1.disabled = true;
   var hojaEstilo2 = document.getElementById("hoja-estilo-2");
   hojaEstilo2.disabled = false;
+}
+
+if (localStorageValue2 === "1") {
+  // Si se ha guardado el valor "1", activar la primera hoja de estilo
+  var menu1 = document.getElementById("menu-1");
+  menu1.disabled = false;
+  var menu2 = document.getElementById("menu-2");
+  menu2.disabled = true;
+} else {
+  // Si se ha guardado el valor "2", activar la segunda hoja de estilo
+  var menu1 = document.getElementById("menu-1");
+  menu1.disabled = true;
+  var menu2 = document.getElementById("menu-2");
+  menu2.disabled = false;
 }
